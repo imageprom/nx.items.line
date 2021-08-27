65e7b158da26da3a7a4da572e6f522cb62751d00
Imageprom <42964807+imageprom@users.noreply.github.com>
2021-08-27 12:00:51 +0300

    Update .description.php

--- nx.items.line_1.0/.description.php
+++ nx.items.line_1.0/.description.php
@@ -2,8 +2,8 @@
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
 
 $arComponentDescription = array(
-	"NAME" => GetMessage("T_IBLOCK_DESC_LINE"),
-	"DESCRIPTION" => GetMessage("T_IBLOCK_DESC_LINE_DESC"),
+	"NAME" => GetMessage('IP_COMPONENT_NAME'),
+	"DESCRIPTION" => GetMessage('IP_COMPONENT_DESC'),
 	"ICON" => "/images/news_line.gif",
 	"SORT" => 10,
 	"CACHE_PATH" => "Y",
