dd219786d32abf615b6c7e38210556043c36e0c0
Imageprom <42964807+imageprom@users.noreply.github.com>
2021-08-27 12:01:41 +0300

    Update .description.php

--- nx.items.line_1.0/lang/en/.description.php
+++ nx.items.line_1.0/lang/en/.description.php
@@ -2,4 +2,9 @@
 $MESS ['T_IBLOCK_DESC_LINE'] = "Articles and news feed";
 $MESS ['T_IBLOCK_DESC_LINE_DESC'] = "List of articles and news with the date and title";
 $MESS ['T_IBLOCK_DESC_NEWS'] = "Articles and news";
+
+$MESS ['IP_COMPONENT_NAME'] = "IB Element List Map 1.0";
+$MESS ['IP_COMPONENT_DESC'] = "Displays IB element";
+$MESS ['IP_COMPONENTS_TITLE'] = "Imageprom";
+$MESS ['IP_COMPONENTS_GROUP'] = "IB";
 ?>
